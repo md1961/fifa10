d80060606bb00379f63ec03a1e30a648f9862eca
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2009-12-19 09:12:19 +0900

    app/helpers/players_helper.rb - Added helper column_index() and column_align(). app/views/players/list.html.erb - Column indexes and alignments in the table. public/stylesheets/style.css - Added '#table_players th'.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,36 @@
 module PlayersHelper
+
+  COLUMN_ATTRIBUTES = {
+    :id                => ['id'    , :R],
+    :name              => ['name'  , :L],
+    :first_name        => ['fname' , :L],
+    :number            => ['No'    , :R],
+    :position_id       => ['Pos'   , :L],
+    :skill_move        => ['Skill' , :L],
+    :is_right_dominant => ['Ft'    , :L],
+    :both_feet_level   => ['Both'  , :L],
+    :height            => ['H'     , :R],
+    :weight            => ['W'     , :R],
+    :birth_year        => ['Birth' , :R],
+    :nation_id         => ['Nation', :L],
+    :team_id           => ['Team'  , :L],
+  }.freeze
+
+  def column_index(column)
+    return column_attribute(column, 0)
+  end
+
+  DEFAULT_ALIGN = 'left'
+
+  def column_align(column)
+    align = column_attribute(column, 1)
+    return DEFAULT_ALIGN if align.nil?
+    return align == :L ? 'left' : align == :R ? 'right' : 'center'
+  end
+
+  def column_attribute(column, index)
+    attributes = COLUMN_ATTRIBUTES[column.name.intern]
+    return nil if attributes.nil?
+    return attributes[index]
+  end
 end
