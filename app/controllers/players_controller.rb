43763fbb3515645a220a3c9293e87ee593fd55fe
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2009-12-27 11:31:41 +0900

    app/controllers/players_controller.rb - Added method edit(). app/views/players/show.html.erb - Moved the table to _base.html.erb. config/routes.rb - Use map.root in place of map.connect ''. public/stylesheets/style.css - Added style for '#table_player_property th'. app/views/players/_base.html.erb - Newly created, taking over the table from show.html.erb. app/views/players/edit.html.erb - Newly created.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -20,6 +20,12 @@ class PlayersController < ApplicationController
     @page_title = "#{@player.number} #{@player.name}, #{@player.first_name}"
   end
 
+  def edit
+    @player = Player.find(params[:id])
+
+    @page_title = "Editing #{@player.name}, #{@player.first_name} ..."
+  end
+
   def choose_to_list
     @row_filter    = get_row_filter
     @column_filter = get_column_filter
