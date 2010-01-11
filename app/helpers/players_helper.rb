b6eca7c1df0deb56ea95e38afb69fb4231e7827e
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-01-11 10:10:33 +0900

    app/helpers/players_helper.rb - Added method prepare_superior_attribute_values(). - Added method html_class_for_attribute_display(), taking over the   corresponding code from app/views/players/list.html.erb. app/views/players/list.html.erb - Modified to prepare @superior_attribute_values using helper   prepare_superior_attribute_values(). - Moved the corresponding code to helper html_class_for_attribute_display(). public/stylesheets/style.css - Added style for '#table_players .superior_attribute_value'.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -50,6 +50,31 @@ module PlayersHelper
       + ColumnFilter::DEFENSIVE_ATTRIBUTE_NAMES   + ColumnFilter::GOALKEEPING_ATTRIBUTE_NAMES
   end
 
+  def prepare_superior_attribute_values
+    values = Hash.new { |h, k| h[k] = 0 }
+    if @players.size == 2
+      attrs1 = @players[0].player_attribute
+      attrs2 = @players[1].player_attribute
+      @attribute_columns.map(&:name).each do |attr_name|
+        value1 = attrs1.read_attribute(attr_name)
+        value2 = attrs2.read_attribute(attr_name)
+        values[attr_name] = value1 > value2 ? value1 : value2
+      end
+    end
+
+    return values
+  end
+
+  def html_class_for_attribute_display(attr_name, value, is_leftmost)
+    clazz = ''
+    clazz =  'leftmost_of_attributes' if is_leftmost
+    clazz += ' sorted_field'    if @sorted_field_names.include?(attr_name)
+    clazz += ' top_five_values' if value >= @attribute_top_five_values[attr_name]
+    superior_value = @superior_attribute_values[attr_name]
+    clazz += ' superior_attribute_value' if superior_value > 0 and value >= superior_value
+    return clazz
+  end
+
   def command_samples_for_roster_chart
     return [
       "s9 with b2 : Exchange 'Starter 9' with 'Bench 2'",
