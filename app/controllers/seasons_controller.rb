f7a94de3fda33cb4a44876195a00ce5a88a5ef9e
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-01-16 14:55:02 +0900

    (Temporary commit) app/controllers/seasons_controller.rb - Added method list(). app/models/season.rb - Added self.list() and <=>(). app/views/chronicles/list.html.erb - Fixed the action of the link(s) in the loop. public/stylesheets/style.css - Added style for '#season_list'. app/views/seasons/list.html.erb - Newly created.

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,2 +1,7 @@
 class SeasonsController < ApplicationController
+
+  def list
+    chronicle_id = params[:chronicle_id]
+    @seasons = Season.list(chronicle_id)
+  end
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -4,7 +4,17 @@ class Season < ActiveRecord::Base
   has_many :player_seasons
   has_many :players, :through => :player_seasons
 
+  def self.list(chronicle_id)
+    seasons = find_all_by_chronicle_id(chronicle_id)
+    seasons.sort!
+    return seasons
+  end
+
   def year_start
     return years[0, years.index('-')].to_i
+  end
+
+  def <=>(other)
+    return self.year_start.<=>(other.year_start)
   end
 end
