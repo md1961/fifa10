08779ed7c2c13d1ab1962520eb1b41b02007cf76
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-01-18 23:19:54 +0900

    (Temporary commit while creating 'matches/new'.) app/controllers/matches_controller.rb - Added method new(). app/views/matches/list.html.erb - Added a link to 'New Match'. - Moved the body of the table to partial 'list'. app/views/matches/_form.html.erb app/views/matches/_list.html.erb app/views/matches/_match.html.erb app/views/matches/new.html.erb - Newly created.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,4 +6,13 @@ class MatchesController < ApplicationController
 
     @page_title = "#{team_name_and_season_years} Fixtures and Results"
   end
+
+  def new
+    season_id = session[:season_id]
+    @matches = Match.list(season_id)
+    @match = Match.new
+
+    @page_title_size = 3
+    @page_title = "#{team_name_and_season_years} New Fixture/Result"
+  end
 end
