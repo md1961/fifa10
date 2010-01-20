a796c50164e092ff33758b87672615c2ade7195d
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-01-20 23:00:35 +0900

    (Temporary commit while creating 'matches/new'.) app/helpers/matches_helper.rb - Added helper text_field_size(). app/models/series.rb - Added method self.premier_all(). app/views/matches/_form.html.erb - Almost finished, NOT tested yet. db/teams_premier.txt - Corrected data with ManUtd and nation_id.

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,14 @@
 module MatchesHelper
+
+  HASH_TEXT_FIELD_SIZE = {
+    :date_match  => 10,
+    :ground      =>  2,
+    :scorers_own => 20,
+    :scorers_opp => 20,
+  }
+  DEFAULT_TEXT_FIELD_SIZE = 8
+
+  def text_field_size(column_name)
+    return HASH_TEXT_FIELD_SIZE[column_name.intern] || DEFAULT_TEXT_FIELD_SIZE
+  end
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -1,2 +1,17 @@
 class Series < ActiveRecord::Base
+
+  def self.premier_all
+    hash_order = {
+      "Premier"    => 1,
+      "FA Cup"     => 2,
+      "Carling C." => 3,
+      "UEFA CL"    => 4,
+      "UEFA EL"    => 5,
+      "UEFA SC"    => 6,
+      "C. Shield"  => 7,
+    }
+    series = find(:all, :conditions => ["abbr in (?)", hash_order.keys])
+    series.sort! { |s1, s2| hash_order[s1.abbr].<=>(hash_order[s2.abbr]) }
+    return series
+  end
 end
