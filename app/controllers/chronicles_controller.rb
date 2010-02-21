0b48e5edf1f130f10aced27b65834c6d2799fc7f
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-02-21 21:21:05 +0900

    app/controllers/chronicles_controller.rb - Altered list() to order by name. - Added new() and create(). app/views/chronicles/list.html.erb - Added link to 'New Chronicle'. app/views/chronicles/new.html.erb - Newly created. public/stylesheets/style.css - Added style for '#link_new_chronicle' and '#chronicle_examples'.

--- app/controllers/chronicles_controller.rb
+++ app/controllers/chronicles_controller.rb
@@ -4,10 +4,34 @@ class ChroniclesController < ApplicationController
     redirect_to :action => 'list'
   end
 
+  ORDER_BY = "name"
+
   def list
-    @chronicles = Chronicle.find(:all)
+    @chronicles = Chronicle.find(:all, :order => ORDER_BY)
 
     @page_title_size = 2
     @page_title = "Choose a Chronicle (Series of Seasons)"
+  end
+
+  def new
+    @chronicle = Chronicle.new
+
+    prepare_page_title_for_new
+  end
+
+    def prepare_page_title_for_new
+      @page_title_size = 3
+      @page_title = "Creating a New Chronicle"
+    end
+    private :prepare_page_title_for_new
+
+  def create
+    @chronicle = Chronicle.new(params[:chronicle])
+    if @chronicle.save
+      redirect_to :action => 'list'
+    else
+      prepare_page_title_for_new
+      render :action => 'new'
+    end
   end
 end
