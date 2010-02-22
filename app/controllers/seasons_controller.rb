e240b2e425ca342f60e319a6a4d89d3682d47050
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-02-22 21:54:56 +0900

    app/controllers/seasons_controller.rb - Modified list() and new() to prepare @chronicle_id. - Added create(). app/models/season.rb - Added validations. app/views/seasons/list.html.erb - Fixed the link to 'New Season' to bring :chronicle_id. app/views/seasons/new.html.erb - Finished.

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,17 +1,30 @@
 class SeasonsController < ApplicationController
 
   def list
-    chronicle_id = params[:chronicle_id]
-    @seasons = Season.list(chronicle_id)
+    @chronicle_id = params[:chronicle_id]
+    @seasons = Season.list(@chronicle_id)
     
     @page_title_size = 2
-    @page_title = "Chronicle: #{Chronicle.find(chronicle_id).name}"
+    @page_title = "Chronicle: #{Chronicle.find(@chronicle_id).name}"
   end
 
   def new
     @season = Season.new
+    @season.closed = false
+    @chronicle_id = params[:chronicle_id]
 
     prepare_page_title_for_new
+  end
+
+  def create
+    @season = Season.new(params[:season])
+    if @season.save
+      redirect_to :action => 'list', :chronicle_id => @season.chronicle_id
+    else
+      prepare_page_title_for_new
+      @chronicle_id = params[:season][:chronicle_id]
+      render :action => 'new'
+    end
   end
 
     def prepare_page_title_for_new
--- app/models/season.rb
+++ app/models/season.rb
@@ -4,6 +4,10 @@ class Season < ActiveRecord::Base
   has_many :player_seasons
   has_many :players, :through => :player_seasons
 
+  validates_presence_of     :year_start
+  validates_numericality_of :year_start, :only_integer => true, :greater_than_or_equal_to => 1990
+  validates_uniqueness_of   :year_start, :scope => [:chronicle_id, :team_id]
+
   #TODO: many-to-many with Series thru TABLE SeasonSeries
 
   MONTH_START = 7
