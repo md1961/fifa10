441f1ba35e087b68e20c38e1baa76674523efed1
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-05-18 22:57:58 +0900

    Removed Series.premier_all() and Series.premier_all_but_friendly()
    app/models/series.rb
    - Removed self.premier_all() and self.premier_all_but_friendly().
    app/views/matches/list.html.erb
    - Removed commented-out statement.

--- app/models/series.rb
+++ app/models/series.rb
@@ -2,29 +2,6 @@ class Series < ActiveRecord::Base
   has_many :season_series
   has_many :season, :through => :season_series
 
-  #TODO: Delete
-  def self.premier_all
-    hash_order = {
-      # abbr    order in list
-      'Premier'    => 1,
-      'FA Cup'     => 2,
-      'L. Cup'     => 3,
-      'CL'         => 4,
-      #'EL'    => 5,
-      'UEFA SC'    => 6,
-      'C. Shield'  => 7,
-      'Friendly'   => 8,
-    }
-    series = find(:all, :conditions => ["abbr in (?)", hash_order.keys])
-    series.sort! { |s1, s2| hash_order[s1.abbr].<=>(hash_order[s2.abbr]) }
-    return series
-  end
-
-  #TODO: Delete
-  def self.premier_all_but_friendly
-    return premier_all.select { |s| s.abbr != 'Friendly' }
-  end
-
   ABBR_FOR_PREMIER = 'Premier'
 
   def self.premier
