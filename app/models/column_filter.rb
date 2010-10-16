1836564d84e04d8095073f192f0caacf6a1782f9
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-10-16 19:52:17 +0900

    Removed 'note' from ColumnFilter::COLUMN_NAMES_TO_DISPLAY.
    app/models/column_filter.rb
    - Removed 'note' from COLUMN_NAMES_TO_DISPLAY.

--- app/models/column_filter.rb
+++ app/models/column_filter.rb
@@ -31,7 +31,7 @@ class ColumnFilter
   COLUMN_NAMES_NOT_TO_DISPLAY = %w(id order_number)
 
   COLUMN_NAMES_TO_DISPLAY = %w(name first_name number position_id birth_year height weight
-            nation_id is_right_dominant both_feet_level skill_move overall market_value note)
+            nation_id is_right_dominant both_feet_level skill_move overall market_value)
 
   def self.displaying_columns
     return Player.columns.select { |column| ! COLUMN_NAMES_NOT_TO_DISPLAY.include?(column.name) }
