d250ad2ad7b10b5abb90c09836b31a4ee0cfadf0
Naoyuki Kumagai <kumagai@fergus>
2011-02-11 12:03:00 +0900

    First import with Rails 3.0 new project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fifa10::Application.config.secret_token = '459cb74a7a49c61d1ed15818ce1e1fccb9657b9f106d2a40c62e50391a4d89aa3bb15320c9341591c61cc8cde249e1c547399f100e55c13c928493543f087a99'
