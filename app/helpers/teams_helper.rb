1bee6446cb241cc4bc7823a928fd3aa2c6095fad
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2011-02-25 17:58:50 +0900

    Renamed TeamsHelper#column_align() to avoid conflict with PlayersHelper's
    app/helpers/teams_helper.rb
    - Renamed column_align() to column_align_team().
    app/views/teams/index.html.erb
    - TeamsHelper#column_align() was renamed to column_align_team().

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,6 +1,6 @@
 module TeamsHelper
 
-  def column_align(column_name)
+  def column_align_team(column_name)
     return 'right' if %(id, year_founded).include?(column_name)
     return 'left'
   end
