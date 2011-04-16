ef8f5c93733f21e350bf553315a6b3b7105e2e58
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2011-04-16 16:54:44 +0900

    [Tmp] Move get_injury_list() and much more to RosterChartsController
    app/controllers/players_controller.rb
    app/controllers/roster_charts_controller.rb
    - Move get_num_starters_and_in_bench(), get_injury_list(), get_off_list()
      and much more private methods in PlayersController to RosterChartsController.

--- app/controllers/roster_charts_controller.rb
+++ app/controllers/roster_charts_controller.rb
@@ -40,4 +40,434 @@ class RosterChartsController < ApplicationController
     @page_title_size = 3
     @page_title = "#{team_name_and_season_years} Roster Chart"
   end
+
+  private
+
+    def recover_disabled
+      season_id = get_season_id
+      next_match = Match.nexts(season_id).first
+      return unless next_match
+      today = next_match.date_match
+      players = players_of_team(includes_on_loan=false, for_lineup=true)
+      players_recoverd = Array.new
+      players.each do |player|
+        is_recovered = player.recover_from_disabled(today, season_id)
+        players_recoverd << player if is_recovered
+      end
+
+      #TODO: Merge into put_injury_report_into_session()
+      if players_recoverd.size > 0
+        report = "#{players_recoverd.size} player(s) recovered: #{players_recoverd.map(&:name).join(', ')}"
+        report_in_session = session[:roster_chart_report] || ""
+        session[:roster_chart_report] = report_in_session + "<br />" + report
+      end
+    end
+
+    def get_num_starters_and_in_bench
+      num_starters = Constant.get(:num_starters)
+      num_in_bench = Constant.get(:num_in_bench)
+      return num_starters, num_in_bench
+    end
+
+    def get_injury_list
+      get_list_from_simple_db(:injury_list)
+    end
+
+    def set_injury_list(injury_list)
+      set_list_to_simple_db(:injury_list, injury_list)
+    end
+
+    def get_off_list
+      get_list_from_simple_db(:off_list)
+    end
+
+    def set_off_list(off_list)
+      set_list_to_simple_db(:off_list, off_list)
+    end
+
+    def get_list_from_simple_db(name)
+      return SimpleDB.instance.get(name) || Array.new
+    end
+
+    def set_list_to_simple_db(name, list)
+      raise ArgumentError.new("Argument must be an Array") unless list.kind_of?(Array)
+      SimpleDB.instance.set(name, list)
+    end
+
+    ACTION_WITH    = 'with'
+    ACTION_TO      = 'to'
+    ACTION_LOAN    = 'loan'
+    ACTION_INJURE  = 'injure'
+    ACTION_RECOVER = 'recover'
+    ACTION_OFF     = 'off'
+    ACTION_HOT     = 'hot'
+    ACTION_NOTWELL = 'notwell'
+    ACTION_DISABLE = 'disable'
+    ACTION_UNTIL   = 'until'
+    ACTION_SHOW    = 'show'
+
+    LEGAL_ACTIONS = [
+      ACTION_WITH, ACTION_TO, ACTION_LOAN, ACTION_INJURE, ACTION_RECOVER, ACTION_OFF,
+      ACTION_HOT, ACTION_NOTWELL,  ACTION_DISABLE, ACTION_UNTIL, ACTION_SHOW
+    ]
+
+    MAP_NUM_PLAYERS = {
+      ACTION_WITH  => 2,
+      ACTION_TO    => 2,
+      ACTION_UNTIL => 2,
+    }
+
+    def update_roster(commands, players, is_lineup)
+      action, players_arg = commands
+      unless players_arg_legal?(players_arg, action)
+        return
+      end
+      if action == ACTION_LOAN && is_lineup
+        explain_error("Illegal command", ["loan command not allowed in lineup mode"], [])
+        return
+      end
+
+      begin
+        Player.transaction do
+          case action
+          when ACTION_WITH
+            player1, player2 = players_arg
+            exchange_player_order(player1, player2, is_lineup)
+          when ACTION_TO
+            player1, player2 = players_arg
+            insert_player_order_before(player1, player2, players, is_lineup)
+          when ACTION_LOAN
+            loan_player(players_arg)
+          when ACTION_INJURE
+            put_into_injury(players_arg)
+          when ACTION_RECOVER
+            recover_from_injury(players_arg)
+          when ACTION_OFF
+            off_player(players_arg)
+          when ACTION_HOT
+            hot_player(players_arg)
+          when ACTION_NOTWELL
+            not_well_player(players_arg)
+          when ACTION_DISABLE
+            disable_players(players_arg, toggles=true)
+          when ACTION_UNTIL
+            player, days_disabled = players_arg
+            set_disabled_until(player, days_disabled)
+          when ACTION_SHOW
+            show_player_attributes(players_arg)
+          else
+            raise ActiveRecord::Rollback, "Unknown action '#{action}' in helper update_roster()"
+          end
+        end
+      #rescue
+        #explain_error("DB Error", ["Failed database transaction"], [])
+      end
+    end
+
+    def players_arg_legal?(players_arg, action)
+      legal_num_players = MAP_NUM_PLAYERS[action]
+      return true unless legal_num_players
+      is_legal = players_arg.size == legal_num_players
+      unless is_legal
+        msg = "Number of players must be #{legal_num_players} (#{players_arg.size} given)"
+        explain_error("Illegal number of players", [msg], [])
+      end
+      return is_legal
+    end
+
+    def exchange_player_order(player1, player2, is_lineup)
+      season_id = is_lineup ? 0 : get_season_id(params)
+      n1 = player1.order_number(season_id)
+      n2 = player2.order_number(season_id)
+      player1.set_order_number(999999, season_id)
+      player1.save! unless is_lineup
+      player2.set_order_number(n1, season_id)
+      player2.save! unless is_lineup
+      player1.set_order_number(n2, season_id)
+      player1.save! unless is_lineup
+    end
+
+    def insert_player_order_before(player1, player2, players, is_lineup)
+      index1 = players.index(player1)
+      index2 = players.index(player2)
+      return if index1 == index2
+      if index1.nil? || index2.nil?
+        names = Array.new
+        names << player1.name if index1.nil?
+        names << player2.name if index2.nil?
+        raise ActiveRecord::Rollback, "Cannot find Player '#{names.join("', '")}' in 'players'"
+      end
+
+      season_id = is_lineup ? 0 : get_season_id(params)
+      player1 = players[index1]
+      n_prev = player1.order_number(season_id)
+      player1.set_order_number(999999, season_id)
+      player1.save! unless is_lineup
+      step = sgn(index2 - index1)
+      (index1 + step).step(index2, step) do |index|
+        player = players[index]
+        n = player.order_number(season_id)
+        player.set_order_number(n_prev, season_id)
+        player.save! unless is_lineup
+        n_prev = n
+      end
+      player1.set_order_number(n_prev, season_id)
+      player1.save! unless is_lineup
+    end
+
+    def loan_player(players)
+      season_id = get_season_id(params)
+      players.each do |player|
+        on_loan = player.on_loan?(season_id)
+        player.set_on_loan(! on_loan, season_id)
+      end
+    end
+
+    def put_into_injury(players)
+      injury_list = get_injury_list
+      season_id = get_season_id
+
+      players.each do |player|
+        if injury_list.include?(player.id)
+          explain_error("Cannot injure", ["'#{player.name}' is already in injury list"], [])
+          return
+        elsif player.hot?(season_id)
+          explain_error("Cannot injure", ["'#{player.name}' is in hot status"], [])
+          return
+        end
+      end
+
+      players.each do |player|
+        injury_list << player.id
+        set_injury_list(injury_list)
+      end
+
+      put_injury_report_into_session([], players)
+    end
+
+    def recover_from_injury(players)
+      injury_list = get_injury_list
+      season_id = get_season_id
+
+      players.each do |player|
+        is_injured = injury_list.include?(player.id)
+        next unless is_injured || player.disabled?(season_id)
+        if is_injured
+          injury_list.delete(player.id)
+          set_injury_list(injury_list)
+        else
+          player.disable(season_id, true)
+        end
+      end
+    end
+
+    def off_player(players)
+      off_list    = get_off_list
+      injury_list = get_injury_list
+
+      players.each do |player|
+        player_id = player.id
+        next if injury_list.include?(player_id)
+        if off_list.include?(player_id)
+          off_list.delete(player_id)
+        else
+          off_list << player_id
+        end
+      end
+
+      set_off_list(off_list)
+    end
+
+    def hot_player(players)
+      season_id = get_season_id(params)
+      players.each do |player|
+        is_hot = player.hot?(season_id)
+        player.set_hot(! is_hot, season_id)
+      end
+    end
+
+    def not_well_player(players)
+      season_id = get_season_id(params)
+      players.each do |player|
+        is_not_well = player.not_well?(season_id)
+        player.set_not_well(! is_not_well, season_id)
+      end
+    end
+
+    def show_player_attributes(players_arg)
+      row_filter = get_row_filter(nil, for_lineup=true)
+      row_filter.set_no_players
+      players_arg.each do |player|
+        row_filter.set_player_by_name(player.name)
+      end
+
+      set_row_filter(row_filter)
+    end
+
+    NORMAL_TERMS_SIZE = 3
+    RE_ACTION = /\A[a-zA-Z]+\z/
+    PLAYER_FORMAT = "\\d{1,2}"
+    RE_PLAYER = /\A#{PLAYER_FORMAT}\z/
+
+    def parse_roster_edit_command(command, players)
+      if command.blank?
+        explain_error("", ["No command specified"], [])
+        return nil
+      end
+
+      title = "Command \"#{command}\" was illegal"
+      terms = command.split
+      terms.insert(0, ACTION_WITH) if terms.size == 2 && RE_PLAYER =~ terms[0] && RE_PLAYER =~ terms[1]
+      terms.insert(1, terms.shift) unless RE_ACTION =~ terms.first
+
+      action = complete_action(terms.first)
+      return nil unless action
+      unless LEGAL_ACTIONS.include?(action)
+        legal_actions = "'" + LEGAL_ACTIONS.join("', '") + "'"
+        explain_error(title, ["Legal actions are #{legal_actions}"], [])
+        return nil
+      end
+
+      additional_args = Array.new
+      additional_args << terms.pop if action == ACTION_UNTIL
+
+      players_arg = terms2players(terms[1 .. -1], players, title)
+      return nil unless players_arg
+
+      return action, players_arg + additional_args
+    end
+
+    def complete_action(action)
+      candidates = LEGAL_ACTIONS.select { |a| a.starts_with?(action) }
+      if candidates.size >= 2
+        title = "Action \"#{action} is ambiguous"
+        msg   = "Cannot pick from '#{candidates.join('\', \'')}'"
+        explain_error(title, [msg], [])
+        return nil
+      end
+      return candidates.size == 1 ? candidates[0] : action
+    end
+
+    RE_SERIAL_PLAYERS = /\A(#{PLAYER_FORMAT})-(#{PLAYER_FORMAT})\z/
+
+    def terms2players(terms, players, title)
+      terms_here = nil
+      #FIXME: Deactivate?
+      terms_here = parse_serial_players($1, $2) if false && terms.size == 1 && terms[0] =~ RE_SERIAL_PLAYERS
+      terms_here = terms unless terms_here 
+
+      players_from_terms = terms_here.map { |term| term2player(term, players) }
+      return players_from_terms if players_from_terms.all? { |player| player }
+
+      illegal_players = Array.new
+      players_from_terms.each_with_index do |player, index|
+        illegal_players << terms_here[index] unless player
+      end
+      players = "'#{illegal_players.join("' and '")}'"
+      mult = illegal_players.size > 1
+      message = "#{players} #{mult ? 'are' : 'is'} illegal player#{mult ? 's' : ''}"
+      explain_error(title, [message], [])
+
+      return nil
+    end
+
+    def parse_serial_players(term1, term2)
+      kind1 = player_kind(term1)
+      kind2 = player_kind(term2)
+      return nil if kind1 != kind2
+
+      index1 = player_index(term1)
+      index2 = player_index(term2)
+      return nil if index1 > index2
+
+      return (index1 .. index2).to_a.map { |index| "#{kind1}#{index}" }
+    end
+
+    RE_NUMBER = /\A\d+\z/
+
+    def player_kind(term)
+      term = "s#{term}" if term =~ RE_NUMBER
+      kind = term[0, 1]
+      return nil unless %w(s b r).include?(kind)
+      return kind
+    end
+
+    def player_index(term)
+      return term.sub(/\A[sbr]/, "").to_i
+    end
+
+    def term2player(term, players)
+      return players.find { |player| player.number == term.to_i }
+    end
+
+    def sort_players(players, sort_fields)
+      players.sort! { |player1, player2| compare_players(player1, player2, sort_fields) }
+    end
+
+    def compare_players(player1, player2, sort_fields)
+      sort_fields.each do |field|
+        name = field.name
+        return 0 if name.to_s == 'none'
+
+        ascending = field.ascending?
+        value1 = player1.get(name) || 0
+        value2 = player2.get(name) || 0
+        cmp = sgn(value1 - value2) * (ascending ? 1 : -1)
+        return cmp if cmp != 0
+      end
+
+      return 0
+    end
+
+    def parse_commant_to_filter(command)
+      command = command.strip
+      if command.blank?
+        explain_error("No command specified", ["No command specified"], [])
+        return false
+      end
+
+      row_filter = get_row_filter
+
+      title = "Command '#{command}' specified"
+
+      kind = nil
+      names = Array.new
+      command.split.each do |name|
+        name = name.downcase
+        if kind.nil?
+          is_position = RowFilter.position?(name)
+          is_player   = RowFilter.player_name_match(name).size > 0
+          if ! is_position && ! is_player
+            explain_error(title, ["Unknown position/player name '#{name}'"], [])
+            return false
+          end
+          kind = is_position ? 'position' : 'player'
+        end
+        found = kind == 'position' ? RowFilter.position?(name) : RowFilter.player_name_match(name).size > 0
+        unless found
+          explain_error(title, ["Unknown #{kind} name '#{name}'"], [])
+          return false
+        end
+        names << name
+      end
+
+      row_filter.send(:"set_no_#{kind}s")
+      names.each do |name|
+        begin
+          row_filter.send(:"set_#{kind}_by_name", name)
+        rescue RowFilter::CannotFindPlayerException
+          explain_error(title, ["Unknown Player name '#{name}'"], [])
+          return false
+        end
+      end
+
+      set_row_filter(row_filter)
+
+      return true
+    end
+
+    def explain_error(title, texts, lists)
+      session[:error_explanation] = ErrorExplanation.new(title, texts, lists)
+      return
+    end
 end
