c59cdae59fd3cf9aff8b0957a2d12932bcdf708c
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2011-08-22 05:36:04 +0900

    Fix 'players#index' to able to hide note's of players
    app/models/column_filter.rb
    - Add :note to PLAYER_PROPERTY_NAMES.

--- app/models/column_filter.rb
+++ app/models/column_filter.rb
@@ -8,7 +8,7 @@ class ColumnFilter
   # To create this, use the following statement:
   # PLAYER_PROPERTY_NAMES = Player.columns.map { |c| c.name.intern }.select { |n| ! COLUMN_NAMES_NOT_TO_FILTER.include?(n) }
   PLAYER_PROPERTY_NAMES = [:first_name, :number, :position_id, :skill_move, :is_right_dominant, :both_feet_level,
-                           :height, :weight, :birth_year, :nation_id, :overall, :market_value, :wage, :age_add_inj]
+                           :height, :weight, :birth_year, :nation_id, :overall, :market_value, :wage, :age_add_inj, :note]
 
   PLAYER_ATTRIBUTE_COLUMNS = PlayerAttribute.content_columns
   PLAYER_ATTRIBUTE_NAMES = PLAYER_ATTRIBUTE_COLUMNS.map { |column| column.name.intern }
