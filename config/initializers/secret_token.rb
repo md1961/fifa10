90495329987a551ba3d8835de17c31c3ac54273d
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2011-10-16 07:40:03 +0900

    Correct to keep necessary code for the project
    Gemfile
    - Keep 'mysql2'.
    Gemfile.lock
    - Removed.
    README
    Rakefile
    app/assets/images/rails.png
    app/assets/javascripts/application.js
    app/assets/stylesheets/application.css
    app/models/.gitkeep
    config/boot.rb
    config/initializers/wrap_parameters.rb
    config/locales/en.yml
    db/seeds.rb
    test/fixtures/.gitkeep
    test/functional/.gitkeep
    test/integration/.gitkeep
    test/performance/browsing_test.rb
    vendor/assets/stylesheets/.gitkeep
    - Use the file from Rails 3.1.1 new.
    config/application.rb
    config/environments/development.rb
    config/environments/production.rb
    config/environments/test.rb
    config/initializers/secret_token.rb
    - Use the file from Rails 3.1.1 new except application name.
    config/database.yml
    config/initializers/session_store.rb
    - Use the original file except comments.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fifa10::Application.config.secret_token = '459cb74a7a49c61d1ed15818ce1e1fccb9657b9f106d2a40c62e50391a4d89aa3bb15320c9341591c61cc8cde249e1c547399f100e55c13c928493543f087a99'
+Fifa10::Application.config.secret_token = '6701cfa49f107737c79340f4f8366ba9227205102a60d9e4495bf192475cb5bdddec9892da7efb2a3193d9e1e0e6a40345ec9405eb9e93ba1bc95a682fa1756d'
